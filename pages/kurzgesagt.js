b75b2944fdd98c71b432bd226fafb87508414c94
lehmannerich <lehmannerich@live.de>
2023-04-14 12:59:48 +0200

    Copywriting improvements

--- pages/kurzgesagt.js
+++ pages/kurzgesagt.js
@@ -13,8 +13,7 @@ export default function Kurzgesagt({}) {
         <TextBlock>
           <p>ich bin ein großer Fan.</p>
           <p>
-            Meine YouTube History zeigt das erste Video, welches meine Neugierde entfacht
-            hat:
+            Die YouTube History zeigt das erste Video, welches ich von Euch entdeckt habe:
           </p>
           <Image
             src="/YT.png"
@@ -45,41 +44,42 @@ export default function Kurzgesagt({}) {
           Ich will euch dabei helfen, Dinge zu schaffen, die eure Fans lieben.
         </TextBlock>
         <TextBlock>
-          Der Erfolg des Shops ist wirklich großartig! Es gibt Zeitpläne zu erstellen und
-          Lieferanten zu verwalten, KPIs zu berechnen… alles mit genügend Weitblick und
-          Fürsorge, damit reichlich Platz für Kreativität bleibt. Ich denke, dass ich das
-          gut hinbekommen würde.
+          Der Erfolg des Shops ist wirklich großartig! Aber das bringt natürlich auch
+          viele Aufgaben mit sich. Es gibt Zeitpläne zu erstellen, Lieferanten zu managen,
+          KPIs zu berechnen… alles mit reichlich Weitblick und Fürsorge, damit reichlich
+          Platz für Kreativität bleibt. Ich denke, dass ich das gut hinbekommen würde.
         </TextBlock>
         <TextBlock>
           Aber noch wichtiger ist, wenn Ihr mir den Vorschlag gestattet, dass ich gut
-          darin wäre Produkte zu bauen die eine wiederkehrende Einnahmequelle schaffen.
-          Offensichtlich habt ihr Fans die euch richtig toll finden und euch regelmäßig,
-          monatlich oder jährlich, unterstützen wollen. Man sieht es am Patreon account.
-          Es ist nich unvorstellbar ein kurzgesagt Produkt zu entdecken, dass sogar den
-          Kalender überholt. Wer weiß, vielleicht ist das Game an dem ihr arbeitet ja
-          schon genau das. In jedem Fall denke ich, dass es noch viel zu Experimentieren
-          und Entdecken gibt.
+          darin wäre Produkte zu bauen, die eine wiederkehrende Einnahmequelle schaffen.
+          Ihr habt Fans, die euch richtig toll finden und euch regelmäßig, monatlich oder
+          jährlich, unterstützen wollen. Man sieht es am Patreon Account. Es ist nicht
+          undenkbar ein kurzgesagt Produkt zu entwickeln, dass sogar euren Kalender
+          überholt. Wer weiß, vielleicht ist das Game an dem ihr gerade arbeitet ja schon
+          genau das. In jedem Fall gibt es hier viele Chancen.
         </TextBlock>
         <Headline>Produktentwicklung ist etwas, das ich wirklich gut kann.</Headline>
         <TextBlock>
-          Ich baue seit über 10 Jahren Produkte - zuerst ganz alleine, neben dem Studium,
+          Ich baue seit über 10 Jahren Produkte - zuerst ganz alleine neben dem Studium,
           und dann ganz professionell mit einem Team und echten Kunden. Das erfolgreichste
-          Produkt, das ich gebaut habe, ist MeetAnyway, eine Plattform für
+          Produkt das ich gebaut habe ist MeetAnyway, eine Plattform für
           Online-Veranstaltungen. Wir hatten über 30.000 Teilnehmer und haben bis zu
-          40.000 € pro Veranstaltung berechnet. Ich habe MeetAnyway mit zwei Freunden
+          40.000 € pro Veranstaltung verdient. Ich habe MeetAnyway mit zwei Freunden
           gegründet und war hauptsächlich für Produkt und Sales verantwortlich. Wir wurden
           vom Y Combinator aufgenommen. YC hat mir nochmal einen Wissensschub gegeben
           darin, wie man Produkte baut, die Menschen wirklich haben wollen.
         </TextBlock>
         <TextBlock>
-          Letztendlich können nur eure Fans sagen, was die richtigen Next Steps sind. Ich
-          wette, dass es manchmal einen Trade-Off gibt zwischen eurer Mission
+          Letztendlich können nur eure Fans sagen, was die richtigen nächsten Schritte
+          sind. Ich wette, dass es manchmal einen Konflikt gibt zwischen eurer Mission
           (Wissenschaft für alle frei zugänglich zu machen) und harten wirtschaftlichen
           Realitäten (60 Personen, die ihren Lebensunterhalt verdienen müssen). Die
-          Herausforderung eines Project Managers besteht hier darin, Wege zu finden, Geld
-          zu verdienen, ohne jemals, auch nur im Geringsten, das Vertrauen zu
-          beeinträchtigen, das kurzgesagt bei seinen Fans aufgebaut hat. Vor allem als ein
-          solcher Fan würde ich die Stelle gerne in Angriff nehmen.
+          Herausforderung eines Project Managers besteht hier darin Geld zu verdienen ohne
+          jemals auch nur im Geringsten das Vertrauen zu beeinträchtigen, das kurzgesagt
+          bei seinen Fans aufgebaut hat.
+        </TextBlock>
+        <TextBlock>
+          Als ein solcher Fan würde ich die Herausforderung gerne in Angriff nehmen.
         </TextBlock>
         <TextBlock>Viele Grüße</TextBlock>
         <TextBlock>Erich Lehmann</TextBlock>
