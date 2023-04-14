417e60a0608c5767ea3fe1ea126872f5d4890bf2
lehmannerich <lehmannerich@live.de>
2023-04-14 11:15:43 +0200

    Full German Version

--- pages/kurzgesagt.js
+++ pages/kurzgesagt.js
@@ -45,10 +45,10 @@ export default function Kurzgesagt({}) {
           Ich will euch dabei helfen, Dinge zu schaffen, die eure Fans lieben.
         </TextBlock>
         <TextBlock>
-          Der Erfolg des Kalenders und des Shops ist wirklich großartig! Es gibt Zeitpläne
-          zu erstellen und Lieferanten zu verwalten, KPIs zu berechnen… alles mit genügend
-          Weitblick und Fürsorge, damit reichlich Platz für Kreativität bleibt. Ich denke,
-          dass ich das gut hinbekommen würde.
+          Der Erfolg des Shops ist wirklich großartig! Es gibt Zeitpläne zu erstellen und
+          Lieferanten zu verwalten, KPIs zu berechnen… alles mit genügend Weitblick und
+          Fürsorge, damit reichlich Platz für Kreativität bleibt. Ich denke, dass ich das
+          gut hinbekommen würde.
         </TextBlock>
         <TextBlock>
           Aber noch wichtiger ist, wenn Ihr mir den Vorschlag gestattet, dass ich gut
@@ -56,10 +56,33 @@ export default function Kurzgesagt({}) {
           Offensichtlich habt ihr Fans die euch richtig toll finden und euch regelmäßig,
           monatlich oder jährlich, unterstützen wollen. Man sieht es am Patreon account.
           Es ist nich unvorstellbar ein kurzgesagt Produkt zu entdecken, dass sogar den
-          Shop überholt UND wiederkehrend Einnahmen generiert. Wer weiß, vielleicht ist
-          das Game an dem ihr arbeitet ja schon genau das. In jedem Fall denke ich, dass
-          es noch viel zu Experimentieren und Entdecken gibt.
+          Kalender überholt. Wer weiß, vielleicht ist das Game an dem ihr arbeitet ja
+          schon genau das. In jedem Fall denke ich, dass es noch viel zu Experimentieren
+          und Entdecken gibt.
         </TextBlock>
+        <Headline>Produktentwicklung ist etwas, das ich wirklich gut kann.</Headline>
+        <TextBlock>
+          Ich baue seit über 10 Jahren Produkte - zuerst ganz alleine, neben dem Studium,
+          und dann ganz professionell mit einem Team und echten Kunden. Das erfolgreichste
+          Produkt, das ich gebaut habe, ist MeetAnyway, eine Plattform für
+          Online-Veranstaltungen. Wir hatten über 30.000 Teilnehmer und haben bis zu
+          40.000 € pro Veranstaltung berechnet. Ich habe MeetAnyway mit zwei Freunden
+          gegründet und war hauptsächlich für Produkt und Sales verantwortlich. Wir wurden
+          vom Y Combinator aufgenommen. YC hat mir nochmal einen Wissensschub gegeben
+          darin, wie man Produkte baut, die Menschen wirklich haben wollen.
+        </TextBlock>
+        <TextBlock>
+          Letztendlich können nur eure Fans sagen, was die richtigen Next Steps sind. Ich
+          wette, dass es manchmal einen Trade-Off gibt zwischen eurer Mission
+          (Wissenschaft für alle frei zugänglich zu machen) und harten wirtschaftlichen
+          Realitäten (60 Personen, die ihren Lebensunterhalt verdienen müssen). Die
+          Herausforderung eines Project Managers besteht hier darin, Wege zu finden, Geld
+          zu verdienen, ohne jemals, auch nur im Geringsten, das Vertrauen zu
+          beeinträchtigen, das kurzgesagt bei seinen Fans aufgebaut hat. Vor allem als ein
+          solcher Fan würde ich die Stelle gerne in Angriff nehmen.
+        </TextBlock>
+        <TextBlock>Viele Grüße</TextBlock>
+        <TextBlock>Erich Lehmann</TextBlock>
       </Main>
     </>
   );
