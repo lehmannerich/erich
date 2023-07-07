db2ed927f56ab4de8a80ae1f873bb1b20277b19d
lehmannerich <lehmannerich@live.de>
2023-07-07 12:16:33 +0200

    Added title

--- pages/podcast.js
+++ pages/podcast.js
@@ -1,42 +1,56 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Kurzgesagt({}) {
   return (
-    <div className="bg-black">
-      <main className="mx-auto max-w-5xl bg-black text-white py-24">
-        <div className=" flex">
-          <div place="left" className="grid content-center ">
-            <Image src="/lehmannpod.svg" alt="Lehmann Podcast" width={300} height={100} />
-            <div className="">Conversations about philosophy, science and business.</div>
-            <div className="flex gap-2 mt-4">
-              {platforms.map((platform) => (
-                <a
-                  key={platform.name}
-                  href={platform.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="py-3 px-14 bg-zinc-800 rounded text-center hover:bg-slate-700 transition-all"
-                >
-                  <p className="mb-1 font-medium text-sm">Watch on</p>
-                  <Image
-                    src={platform.logo}
-                    alt={platform.name}
-                    width={100}
-                    height={50}
-                  />
-                </a>
-              ))}
+    <>
+      <Head>
+        {/* Make a dot in between Erich Lehmann and CV */}
+        <title>Lehmann Podcast</title>
+      </Head>
+      <div className="bg-black">
+        <main className="mx-auto max-w-5xl bg-black text-white py-24">
+          <div className=" flex">
+            <div place="left" className="grid content-center ">
+              <Image
+                src="/lehmannpod.svg"
+                alt="Lehmann Podcast"
+                width={300}
+                height={100}
+              />
+              <div className="text-lg">
+                Conversations about philosophy, science and business.
+              </div>
+              <div className="flex gap-2 mt-8">
+                {platforms.map((platform) => (
+                  <a
+                    key={platform.name}
+                    href={platform.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="py-3 px-14 bg-zinc-800 rounded text-center hover:bg-slate-700 transition-all"
+                  >
+                    <p className="mb-1 font-medium text-sm">Watch on</p>
+                    <Image
+                      src={platform.logo}
+                      alt={platform.name}
+                      width={100}
+                      height={50}
+                    />
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div
+              place="right"
+              className="max-w-sm grid content-center justify-center mx-auto"
+            >
+              <img src="/erich-lehmann-darkbg.jpg" alt="Erich Lehmann" />
             </div>
           </div>
-          <div
-            place="right"
-            className="max-w-sm grid content-center justify-center mx-auto"
-          >
-            <img src="/erich-lehmann-darkbg.jpg" alt="Erich Lehmann" />
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
