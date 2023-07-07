f19afeee3428e7d3fd54b21b18c6f9187d65dec5
lehmannerich <lehmannerich@live.de>
2023-07-07 12:02:49 +0200

    Hero finished (not responsive)

--- pages/podcast.js
+++ pages/podcast.js
@@ -3,14 +3,29 @@ import Image from "next/image";
 export default function Kurzgesagt({}) {
   return (
     <div className="bg-black">
-      <main className="mx-auto max-w-5xl bg-black text-white">
+      <main className="mx-auto max-w-5xl bg-black text-white py-24">
         <div className=" flex">
-          <div place="left" className="grid content-center">
+          <div place="left" className="grid content-center ">
             <Image src="/lehmannpod.svg" alt="Lehmann Podcast" width={300} height={100} />
             <div className="">Conversations about philosophy, science and business.</div>
-            <div className="flex">
-              <div className="">Watch on Spotify</div>
-              <div>Watch on YouTube</div>
+            <div className="flex gap-2 mt-4">
+              {platforms.map((platform) => (
+                <a
+                  key={platform.name}
+                  href={platform.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="py-3 px-14 bg-zinc-800 rounded text-center hover:bg-slate-700 transition-all"
+                >
+                  <p className="mb-1 font-medium text-sm">Watch on</p>
+                  <Image
+                    src={platform.logo}
+                    alt={platform.name}
+                    width={100}
+                    height={50}
+                  />
+                </a>
+              ))}
             </div>
           </div>
           <div
@@ -24,3 +39,16 @@ export default function Kurzgesagt({}) {
     </div>
   );
 }
+
+const platforms = [
+  {
+    name: "Spotify",
+    url: "https://open.spotify.com/show/3Upxs0s95HvbKHqLld5UrO?si=b4509a8c2eb74266",
+    logo: "/logos/spotify.svg",
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com/channel/UCpSRQYhdyxc-wSa-zpeCKfw",
+    logo: "/logos/youtube.svg",
+  },
+];
